199e89a150b6f9079c3f4f3687637bbc74a964bd
Nazarii <n.smorzhevskyi@andersenlab.com>
2020-11-06 16:29:25 +0200

    [feat]: add DdContext, Identity, UserRepository, InitialMigration

---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        
+    }
+}
---
+++ Persistence/Repository Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Persistence.Repository_Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<ApplicationUser>> GetUsers();
+        Task<ApplicationUser> GetUserById(int userId);
+        void Create(ApplicationUser user);
+        void Update(ApplicationUser user);
+        void Delete(int userId);
+    }
+}
