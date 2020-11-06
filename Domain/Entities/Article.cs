f87e093f37fabf5fd2b0640014def83dfef734fc
Vladlen F <v.filippov@andersenlab.com>
2020-11-06 16:18:03 +0200

    [trans]: add 'IArticle' interface & 'Article' entity

---
+++ Domain/Entities/Article.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entity_Interfaces;
+
+namespace Domain.Entities
+{
+    public class Article : IArticle
+    {
+        public int Article_ID { get; set; }
+        public string Article_Text { get; set; }
+        public int Article_Karma { get; set; }
+        public int Author_ID { get; set; }
+    }
+}
---
+++ Domain/Entity Interfaces/IArticle.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entity_Interfaces
+{
+    public interface IArticle
+    {
+        int Article_ID { get; set; }
+        string Article_Text { get; set; }
+        int Article_Karma { get; set; }
+        int Author_ID { get; set; }
+    }
+}
