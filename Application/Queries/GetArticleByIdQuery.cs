6366b2c8c8d7303be4e63e6dc09c30d7e3c00ada
Vladlen F <v.filippov@andersenlab.com>
2020-11-07 12:41:31 +0200

    [trans]: add Article query classes: 'GetArticleByIdQuery' & 'GetAllArticlesQuery'

---
+++ Application/Queries/GetArticleByIdQuery.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using MediatR;
+using Persistence.Repository_Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetArticleByIdQuery : IRequest<Article>
+    {
+        public int Id { get; set; }
+
+        public class GetArticleByIdHandler : IRequestHandler<GetArticleByIdQuery, Article>
+        {
+            private readonly IArticleRepository _articleRepository;
+
+            public GetArticleByIdHandler(IArticleRepository articleRepository)
+            {
+                this._articleRepository = articleRepository;
+            }
+
+            public async Task<Article> Handle(GetArticleByIdQuery query, CancellationToken cancellationToken)
+            {
+                Task<Article> article = _articleRepository.GetArticle(query.Id);
+                if (article == null) return null;
+                return await article;
+            }
+        }
+    }
+}
