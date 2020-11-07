969e53a252373ad0952992dc96432e16fe81a21e
Vladlen F <v.filippov@andersenlab.com>
2020-11-07 12:30:55 +0200

    [trans]: add Article command classes: 'CreateArticleCommand', 'DeleteArticleCommand', 'UpdateArticleCommand' & install MediatR DI

---
+++ Application/Commands/CreateArticleCommand.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using MediatR;
+using Persistence.Repository_Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public class CreateArticleCommand : IRequest<Task>
+    {
+        public int Article_ID { get; set; }
+        public string Article_Text { get; set; }
+        public int Article_Karma { get; set; }
+        public int Author_ID { get; set; }
+
+        public class CreateArticleCommandHandler : IRequestHandler<CreateArticleCommand, Task>
+        {
+            private readonly IArticleRepository _articleRepository;
+
+            public CreateArticleCommandHandler(IArticleRepository articleRepository)
+            {
+                this._articleRepository = articleRepository;
+            }
+
+            public async Task<Task> Handle(CreateArticleCommand command, CancellationToken cancellationToken)
+            {
+                var article = new Article
+                {
+                    Article_ID = command.Article_ID,
+                    Article_Text = command.Article_Text,
+                    Article_Karma = command.Article_Karma,
+                    Author_ID = command.Author_ID
+                };
+
+                await this._articleRepository.Create(article);
+
+                return default;
+            }
+        }
+    }
+}
---
+++ Application/Commands/DeleteArticleCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Persistence.Repository_Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public class DeleteArticleCommand : IRequest<Task>
+    {
+        public int Article_ID { get; set; }
+
+        public class DeleteArticleCommandHandler : IRequestHandler<DeleteArticleCommand, Task>
+        {
+            private readonly IArticleRepository _articleRepository;
+
+            public DeleteArticleCommandHandler(IArticleRepository articleRepository)
+            {
+                this._articleRepository = articleRepository;
+            }
+
+            public async Task<Task> Handle(DeleteArticleCommand command, CancellationToken cancellationToken)
+            {
+                var article = this._articleRepository.GetArticle(command.Article_ID);
+
+                if (article != null)
+                {
+                    await this._articleRepository.Delete(command.Article_ID);
+                    return Task.CompletedTask;
+                }
+                else
+                {
+                    return default;
+                }
+            }
+        }
+    }
+}
